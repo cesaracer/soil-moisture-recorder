6e0308d5ca61bffc6a5aec3b3feaf73c94157e6d
devramoscesar <devramoscesar@gmail.com>
2025-02-20 13:12:46 -0700

    rewrote script

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ load_dotenv()
 i2c_bus = board.I2C()
 ss = Seesaw(i2c_bus, addr=0x36)
 
+headers = {"Content-Type": "application/json; charset=utf-8"}
+url = os.getenv('API_URI')
+
 count = 0
 
 while(count < 10):
@@ -21,21 +24,25 @@ while(count < 10):
     temp = ss.get_temp()
 
     #Getting current date
-    now = str(datetime.now())
+    now = datetime.now()
+    year = now.year
+    month = now.month
+
+    #Formatting month
+    if(month < 10):
+        month = f"0{month}"
 
     #Formatting request body
     data = {
-        "date": now,
+        "date": str(now),
         "moisture": moisture,
-        "temperature": temp
+        "temperature": rount(((temp * 1.8) + 32), 2),
+        "month_year": f"{month}-{year}"
     }
-
-    url = os.getenv('API_URI')
 
     #attempting api request
     try:
-        headers = {"Content-Type": "application/json; charset=utf-8"}
-        response = requests.post(url, headers=headers, json=data)
+        requests.post(url, headers=headers, json=data)
 
         count += 1
         time.sleep(3)
